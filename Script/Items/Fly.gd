52f009c04959b930ebd0d15e5c4a1a4c29e815fb
Velasiraptor <129332397+Velasiraptor@users.noreply.github.com>
2023-07-07 20:33:42 +0300

    Update Fly.gd

--- Script/Items/Fly.gd
+++ Script/Items/Fly.gd
@@ -1,29 +1,26 @@
 extends Node2D
 
-var ind = 0
+var ind = 1
 
 func _ready():
 	$Fly/Fly/Flyanim.play("idleFly")
 
-func _physics_process(delta):
-	if Input.is_action_just_pressed("player_take") and ind == 1:
-		$Fly/Fly.visible = false
+
+func _on_Fly_body_entered(body):
+	if ind == 1:
 		$Press_E.visible = false
+		$AudioFly.play()
+		ind = 0
+		$Fly/Fly.visible = false
 		$Fly/CollisionShape2D.disabled = true
 		get_tree().call_group("Player", "heal")
 		get_tree().call_group("Player", "heal")
-		$AudioFly.play()
 		$Timer.start()
 
-func _on_Fly_body_entered(body):
-	if body.has_method("heal") and body.lives != 6:
-		ind = 1
-		$Press_E.visible = true
-func _on_fly_body_exited(body):
-	ind = 0
-	$Press_E.visible = false
 func _on_Timer_timeout():
 	queue_free()
 
-
-
+func _on_area_e_body_entered(body):
+	$Press_E.visible = true
+func _on_area_e_body_exited(body):
+	$Press_E.visible = false
