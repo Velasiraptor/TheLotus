8c30da4fc715ee4cbc56a6f29dfe19116c8accf6
Velasiraptor <mishuta.maus@mail.ru>
2024-03-06 22:44:00 +0300

    Water_lily
    Сделал дэфолтные кувшинки + рванную кувшинку

--- Scene/SwampLevel1/barrel_of_oil_3p/barrel_of_oil_3p.gd
+++ Scene/SwampLevel1/barrel_of_oil_3p/barrel_of_oil_3p.gd
@@ -1,8 +1,14 @@
 extends AnimatableBody2D
 
+
 func _on_area_player_body_entered(body):
 	if body.has_method("hurt"):
+		$Area_player/Animation_Player_on_barrel.stop()
 		$Area_player/Animation_Player_on_barrel.play("Animation_Player_on_barrel")
+		$Water_drops/Animation_water_drops.play("water_drops")
 		$Animation_oil.stop()
-		await get_tree().create_timer(2.1).timeout
-		$Animation_oil.play("water_animation")
+		$Timer.stop()
+		$Timer.start()
+
+func _on_timer_timeout():
+	$Animation_oil.play("water_animation")
