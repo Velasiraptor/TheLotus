1525daf76476f5c35d01d5394ffd86a625dbd583
Velasiraptor <mishuta.maus@mail.ru>
2024-04-30 22:47:02 +0300

    Inventory

--- Scene/Cave/CaveLVL.gd
+++ Scene/Cave/CaveLVL.gd
@@ -14,17 +14,10 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		last_mouse_motion != OS.get_static_memory_peak_usage()
 		$TimerCursor.start()
-	if event.is_action_pressed("ui_cancel") and $GameOver/GameOver.visible == false and $Inventory/inventory.visible == false:
+	if event.is_action_pressed("ui_cancel") and $GameOver/GameOver.visible == false:
 		get_tree().call_group("Menu", "pause")
-	if event.is_action_pressed("open_inventory") and $GameOver/GameOver.visible == false and $Escape/ESCAPE.visible == false:
-		get_tree().call_group("inventory", "wind2")
-		if $Inventory/inventory.visible == false:
-			$Inventory/inventory.visible = true
-		elif $Inventory/inventory.visible == true:
-			$Inventory/inventory.visible = false
 	if $GameOver/GameOver.visible == true:
 		$Escape/ESCAPE.visible = false
-		$Inventory/inventory.visible = false
 
 func _on_timer_cursor_timeout():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
--- Scene/Player/GUI.gd
+++ Scene/Player/GUI.gd
@@ -1,16 +1,28 @@
 extends CanvasLayer
 
 @onready var animation_icon = %AnimationIcon
+@onready var inventory_slot_1 = %Inventory_slot_1
+@onready var inventory_slot_2 = %Inventory_slot_2
+@onready var inventory_slot_3 = %Inventory_slot_3
+@onready var inventory_slot_4 = %Inventory_slot_4
+@onready var animation_inventory = %Animation_inventory
+@onready var timer_hide_inventory = %Timer_hide_inventory
+
 
 var HP_zero : Texture2D = preload("res://Sprites/UI/GUI/HpZero.png")
 var HP_full : Texture2D = preload("res://Sprites/UI/GUI/HpFull.png")
 var HP_half : Texture2D = preload("res://Sprites/UI/GUI/HpHalf.png")
+
+var ind_inventory = 0
+var anim_name_show = "Animation_inventory_show"
+var anim_name_hide = "Animation_inventory_hide"
 
 var ind_death := false #проверка смерти
 
 func _ready():
 	pass
 
+#ХП и Плеер бар
 
 func create_max_icon_hp_texture() -> TextureRect: #создание иконок макс хп
 	var texture_rect := TextureRect.new()
@@ -86,3 +98,47 @@ func DMGIcon():
 	animation_icon.play("TakeDamage")
 func DeathIcon():
 	animation_icon.play("Death")
+
+
+# ИНВЕНТАРЬ
+
+func _input(event): # Работа клавиш
+	if event.is_action_pressed("Inventory_slot_1"):
+		inventory_slot_1.grab_focus()
+		timer_hide_inventory.stop()
+		timer_hide_inventory.start()
+		if not animation_inventory.is_playing() and ind_inventory == 0:
+			animation_inventory.play("Animation_inventory_show")
+			
+	if event.is_action_pressed("Inventory_slot_2"):
+		inventory_slot_2.grab_focus()
+		timer_hide_inventory.stop()
+		timer_hide_inventory.start()
+		if not animation_inventory.is_playing() and ind_inventory == 0:
+			animation_inventory.play("Animation_inventory_show")
+			
+	if event.is_action_pressed("Inventory_slot_3"):
+		inventory_slot_3.grab_focus()
+		timer_hide_inventory.stop()
+		timer_hide_inventory.start()
+		if not animation_inventory.is_playing() and ind_inventory == 0:
+			animation_inventory.play("Animation_inventory_show")
+			
+	if event.is_action_pressed("Inventory_slot_4"):
+		inventory_slot_4.grab_focus()
+		timer_hide_inventory.stop()
+		timer_hide_inventory.start()
+		if not animation_inventory.is_playing() and ind_inventory == 0:
+			animation_inventory.play("Animation_inventory_show")
+
+func _on_animation_inventory_animation_finished(anim_name):
+	if anim_name == "Animation_inventory_show":
+		ind_inventory = 1
+	else:
+		ind_inventory = 0
+
+func _on_timer_hide_inventory_timeout():
+	animation_inventory.play("Animation_inventory_hide")
+
+
+
