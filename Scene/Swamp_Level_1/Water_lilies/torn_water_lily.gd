21e616cc2ed190dd304560eb0464cab3edbe936a
Velasiraptor <mishuta.maus@mail.ru>
2024-06-13 23:41:56 +0300

    Waterlily_full_fix

--- Scene/Swamp_Level_1/Water_lilies/torn_water_lily.gd
+++ Scene/Swamp_Level_1/Water_lilies/torn_water_lily.gd
@@ -4,12 +4,14 @@ var ind = 1
 @onready var animation_player_on_torn_water_lily = %Animation_player_on_torn_water_lily
 @onready var animation_torn_water_lily = %Animation_torn_water_lily
 @onready var animation_water_drops = %Animation_water_drops
+@onready var animation_wave = %Animation_wave
 
 
 func _on_area_player_body_entered(body):
 	if body.has_method("hurt") and ind == 1:
 		animation_player_on_torn_water_lily.play("Animation_player_on_torn_water_lily")
 		animation_water_drops.play("Animation_water_drops")
+		animation_wave.play("Wave")
 		animation_torn_water_lily.stop()
 		ind = 0
 		get_tree().call_group("Level1", "spawner_catfish")
--- Scene/Swamp_Level_1/Water_lilies/water_lily_1.gd
+++ Scene/Swamp_Level_1/Water_lilies/water_lily_1.gd
@@ -3,6 +3,7 @@ extends Sprite2D
 @onready var animation_water_lily = %Animation_water_lily
 @onready var animation_player_on_water_lily = %Animation_player_on_water_lily
 @onready var animation_water_drops = %Animation_water_drops
+@onready var animation_wave = %Animation_wave
 
 
 func _on_area_player_body_entered(body):
@@ -11,9 +12,11 @@ func _on_area_player_body_entered(body):
 		animation_player_on_water_lily.play("Animation_player_on_water_lily")
 		animation_water_drops.play("water_drops")
 		animation_water_lily.stop()
+		animation_wave.play("Wave")
 		get_tree().call_group("Level1", "spawner_catfish")
 		$Timer.stop()
 		$Timer.start()
 
 func _on_timer_timeout():
 	animation_water_lily.play("Water_lily")
+
